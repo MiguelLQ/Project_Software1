3520cc3742f8c7b03308b76615149540ba24080f
MiguelLQ <113276195+MiguelLQ@users.noreply.github.com>
2024-12-06 22:38:15 -0500

    Add files via upload

---
+++ Models/Metodpag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace albanaPlayaEst.Models
+{
+    public partial class Metodpag
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CodMetd { get; set; } // Código único del método de pago
+
+        [Required(ErrorMessage = "La descripción del método de pago es obligatoria.")]
+        [StringLength(50, ErrorMessage = "La descripción no puede tener más de 100 caracteres.")]
+        public string DescrMetd { get; set; } // Descripción del método de pago
+        public virtual ICollection<Pago> Pagos { get; set; } = new List<Pago>();
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json;
+
+namespace albanaPlayaEst.Models;
+
+public partial class Usuario
+{
+    [Key]
+    public int Id_user { get; set; }
+    public string Nombre { get; set; }
+    public string Correo { get; set; }
+    public string FotoPerfil { get; set; }
+    public string Clave { get; set; }
+
+    [NotMapped]
+    public string[] Roles
+    {
+        get => string.IsNullOrEmpty(RolesJson) ? Array.Empty<string>() : JsonSerializer.Deserialize<string[]>(RolesJson);
+        set => RolesJson = JsonSerializer.Serialize(value);
+    }
+
+
+    // Propiedad que se guarda en la base de datos
+    public string RolesJson { get; set; }
+}
