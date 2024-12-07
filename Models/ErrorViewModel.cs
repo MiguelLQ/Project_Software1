1a39c3fefd7f2e761f474241d7756dd3b909c9de
MiguelLQ <113276195+MiguelLQ@users.noreply.github.com>
2024-12-07 13:38:07 -0500

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace albanaPlayaEst.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
